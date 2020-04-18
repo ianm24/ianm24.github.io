8d3e00108b5e1acd22c5620ae87a6efc3eb3516c
ianm24 <ianmcdowell24@gmail.com>
2020-04-17 23:53:00 -0400

    added components

--- ianm24-app/src/app/app.module.ts
+++ ianm24-app/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SidebarComponent,
+    BlogListComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
